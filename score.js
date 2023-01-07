487b4e8ff4076a02eedf7d8fe6907ab8bebf0ddb
aryafchandra <chandraryaf@gmail.com>
2023-01-07 09:57:10 +0700

    keanya bisa

--- score.js
+++ score.js
@@ -1,11 +1,17 @@
-
 function getValue(){
     let detail = document.getElementById("detail").value;
-    document.getElementById("test").innerHTML= detail;
-    detail = detail.split(" ")
-    console.log(detail[0])
+    detail = detail.split(/\r?\n/);
+    console.log(detail);
+    // console.log(detail.at(-1))
+    document.getElementById("test").innerHTML= calculateScore(detail);
 }
 
-function calculateScore(){
-    return null;
+function calculateScore(arrayScore){
+    var total = 0;
+    for (const element of arrayScore) {
+        var percentage = element.split("\t").at(-2);
+        var score = element.split("\t").at(-1);
+        total += parseFloat(percentage.slice(0,-1)) / 100 * parseFloat(score);
+      }
+    return total;
 }
